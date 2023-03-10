82abaeebf525b81e3d04b476b8c1897aca91f709
laurd-spada <82173669+laurd-spada@users.noreply.github.com>
2023-03-10 09:55:18 +0100

    enabling socket time out on client

--- src/main/java/starter/Main.java
+++ src/main/java/starter/Main.java
@@ -5,11 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try(Socket socket = new Socket("169.239.188.186", 5000)){
+        try(Socket socket = new Socket("localhost", 5000)) {
+
+            socket.setSoTimeout(5000);
+
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
 
@@ -17,17 +21,18 @@ public class Main {
             String echoString;
             String response;
 
-            do{
+            do {
                 System.out.println("Enter String to be echoed: ");
                 echoString = scanner.nextLine();
 
                 printWriter.println(echoString);
-                if(!echoString.equals("exit")){
+                if (!echoString.equals("exit")) {
                     response = bufferedReader.readLine();
                     System.out.println(response);
                 }
             } while (!echoString.equals("exit"));
-
+        } catch (SocketTimeoutException e){
+            System.out.println("The socket timed out");
         } catch (IOException e){
             System.out.println("Client error: " + e.getMessage());
         }
